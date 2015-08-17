4659883e261accc3d612e861b3f30c8081d73546
Barret J. Nobel <bear.nobel@gmail.com>
2015-08-17 16:58:55 -0400

    Initial Commit

---
+++ Thousand Words/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Thousand Words
+//
+//  Created by OSX on 8/17/15.
+//  Copyright (c) 2015 LebonTech Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Thousand Words/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Thousand Words
+//
+//  Created by OSX on 8/17/15.
+//  Copyright (c) 2015 LebonTech Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
