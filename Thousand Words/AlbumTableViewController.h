294b46a5ecf0ae7bf132e39f26f778712eb0a774
Barret J. Nobel <bear.nobel@gmail.com>
2015-08-17 17:26:17 -0400

    created a new storyboard added a tableVC embedded in nav
    created Album object in core data
    created class from object

---
+++ Thousand Words/AlbumTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  AlbumTableViewController.h
+//  Thousand Words
+//
+//  Created by OSX on 8/17/15.
+//  Copyright (c) 2015 LebonTech Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AlbumTableViewController : UITableViewController
+
+@property (strong,nonatomic) NSMutableArray *cellData;
+
+@end
