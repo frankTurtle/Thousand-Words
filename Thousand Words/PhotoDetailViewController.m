524385c2b5831be1c047037a3bb6ecbe5e8b9ac7
Barret J. Nobel <bear.nobel@gmail.com>
2015-08-18 17:30:24 -0400

    added segue from add filter button to segue to filterVC
    coded up prepareForSegue to pass in the photo to the filterVC

--- Thousand Words/PhotoDetailViewController.m
+++ Thousand Words/PhotoDetailViewController.m
@@ -8,6 +8,7 @@
 
 #import "PhotoDetailViewController.h"
 #import "Photo.h"
+#import "FiltersCollectionViewController.h"
 
 @interface PhotoDetailViewController ()
 
@@ -33,15 +34,22 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
+
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([segue.identifier isEqualToString:@"toFilterSegue"])
+    {
+        if ([segue.destinationViewController isKindOfClass:[FiltersCollectionViewController class]])
+        {
+            FiltersCollectionViewController *targetVC = segue.destinationViewController;
+            targetVC.photo = self.photo;
+        }
+    }
+    
 }
-*/
 
 - (IBAction)addFilterButtonPressed:(id)sender
 {
