d9eefda6d45265f4656760d1b4234c6ff0ed068d
Barret J. Nobel <bear.nobel@gmail.com>
2015-08-18 15:51:35 -0400

    created new UIVC created class to manage it hooked them up

---
+++ Thousand Words/PhotoDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhotoDetailViewController.h
+//  Thousand Words
+//
+//  Created by OSX on 8/18/15.
+//  Copyright (c) 2015 LebonTech Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoDetailViewController : UIViewController
+
+@end
---
+++ Thousand Words/PhotoDetailViewController.m
@@ -0,0 +1,37 @@
+//
+//  PhotoDetailViewController.m
+//  Thousand Words
+//
+//  Created by OSX on 8/18/15.
+//  Copyright (c) 2015 LebonTech Solutions. All rights reserved.
+//
+
+#import "PhotoDetailViewController.h"
+
+@interface PhotoDetailViewController ()
+
+@end
+
+@implementation PhotoDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
