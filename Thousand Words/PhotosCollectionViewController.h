24fd5790d3b3a225763ebe1d5fb051c2bcab9a24
Barret J. Nobel <bear.nobel@gmail.com>
2015-08-18 11:49:08 -0400

    added collection view to storyboard created PhotosCollectionVC class hooked up in storyboard

---
+++ Thousand Words/PhotosCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhotosCollectionViewController.h
+//  Thousand Words
+//
+//  Created by OSX on 8/18/15.
+//  Copyright (c) 2015 LebonTech Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotosCollectionViewController : UICollectionViewController
+
+@end
