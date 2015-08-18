e9ef3ceee01b87223f21c14f35f8b867984f991c
Barret J. Nobel <bear.nobel@gmail.com>
2015-08-18 12:58:55 -0400

    added values to transform to UIImage to data and reverse

--- Thousand Words/PictureDataTransformer.m
+++ Thousand Words/PictureDataTransformer.m
@@ -7,7 +7,33 @@
 //
 
 #import "PictureDataTransformer.h"
+#import <UIKit/UIKit.h>
 
 @implementation PictureDataTransformer
+
+// method to return what kind of class we want the image to be turned into
++(Class)transformedValueClass
+{
+    return [NSData class];
+}
+
+// Method to all the data to be turned back into image
++(BOOL)allowsReverseTransformation
+{
+    return YES;
+}
+
+// Method to transform a UIImage to data
+-(id)transformedValue:(id)value
+{
+    return UIImagePNGRepresentation(value); //.. returns data representation of UIImage
+}
+
+// method to return a UIIMage from the core data data
+-(id)reverseTransformedValue:(id)value
+{
+    UIImage *image = [UIImage imageWithData:value]; //.. returns a UIImage from the data
+    return image;
+}
 
 @end
