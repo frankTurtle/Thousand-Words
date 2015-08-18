8c1bd16ba0c6d3947b3fdd449243fff45fcfe105
Barret J. Nobel <bear.nobel@gmail.com>
2015-08-18 11:59:46 -0400

    created subclass of UICOllectionViewCell added UIImageView property to hold picture

---
+++ Thousand Words/PhotoCollectionViewCell.h
@@ -0,0 +1,15 @@
+//
+//  PhotoCollectionViewCell.h
+//  Thousand Words
+//
+//  Created by OSX on 8/18/15.
+//  Copyright (c) 2015 LebonTech Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoCollectionViewCell : UICollectionViewCell
+
+@property (strong,nonatomic) UIImageView *imageView;
+
+@end
---
+++ Thousand Words/PhotoCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.m
+//  Thousand Words
+//
+//  Created by OSX on 8/18/15.
+//  Copyright (c) 2015 LebonTech Solutions. All rights reserved.
+//
+
+#import "PhotoCollectionViewCell.h"
+
+@implementation PhotoCollectionViewCell
+
+@end
