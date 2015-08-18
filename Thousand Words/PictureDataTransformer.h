c1568ca1aa8a834810f9f97cac1c522bcfe2f915
Barret J. Nobel <bear.nobel@gmail.com>
2015-08-18 12:53:16 -0400

    created picture data transformer to convert UIImage to data for core data storage

---
+++ Thousand Words/PictureDataTransformer.h
@@ -0,0 +1,13 @@
+//
+//  PictureDataTransformer.h
+//  Thousand Words
+//
+//  Created by OSX on 8/18/15.
+//  Copyright (c) 2015 LebonTech Solutions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PictureDataTransformer : NSValueTransformer
+
+@end
